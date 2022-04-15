6f1267c118563b845e933fb240f3e267a0f42c17
ariff <ariffkamski@gmail.com>
2022-04-15 10:04:26 +0800

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return(
+        <header>
+            <h1 className="header1">To do List</h1>
+        </header>
+    )
+}
+
+export default Header;
